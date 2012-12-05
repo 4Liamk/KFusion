c1161303f97077cfcdf8db0e4e6315b4e617f426
Liam Kiemele <liamk@Beast.cs.uvic.ca>
2012-12-04 18:42:44 -0800

    added linear alg main documentation

--- lclblas/main.cpp
+++ lclblas/main.cpp
@@ -13,9 +13,19 @@
 char * MATRIX;
 int LSIZE = 16;
 
-//flags to ensure
-#define DEBUG 0
-#define CHECK 1
+/*
+ This is the linear algebra case study.  It tests 3 fairly simple sets of equatiations and compares the fused vs unfused execution times
+
+ If operating correctly, it will only ouput execution times, the format of which can be inferred in the main method.  It measures both transfer time + execution time as well as just execution time
+
+ if incorrect, it will print out and error message with the expected and incorrect value.  These tests are inheriently self testing in way.
+
+ More information on these tests can be found here: https://github.com/4Liamk/KFusion/wiki/Linear-algebra
+*/
+
+//flags to ensure correctness
+#define DEBUG 0 //debug flag will make execution verbose if 1
+#define CHECK 1 //check flag will check all results to ensure they are correct.  If incorrect, it will print out the incorrect matching pair.  Enabled by default currently
 
 //Simple helper function for getting the time as a double
 double gettime()
@@ -27,36 +37,54 @@ double gettime()
 
 /*
   Low Dependency Case Fused
-  This calculates
+  This calculates the pythagoran theorem.
+  The other test cases are very similar in design and follow similiar logic as well as setup and teardown requirements
 */
 int trianglefused(int size)
 {
 	if(DEBUG) perror("Triangle");
+	//initial setup: create three vectors
 	Vector * a = newVector(size,16);
 	Vector * b = newVector(size,16);
 	Vector * c = newVector(size,16);
+
+	//set the vectors to a known value
 	for(int i = 0; i < size; i++)
 	{
 		a->cpu_vals[i] = i;
 		b->cpu_vals[i] = i;
 	}
+
+	//begin timing of execution and transfer to GPU
 	double outerStart = gettime();
 	vector_transfer(a,1);
 	vector_transfer(b,1);
+		
+		//finish transfer
 		clFinish(queue);
 		double innerStart = gettime();
+		
+		//execute operation, in this case they are fused by the addition of the #pragmas
 		#pragma startfuse
 		vector_gpu_mult(a,1,a,1,a);
 		vector_gpu_mult(b,1,b,1,b);
 		vector_gpu_add (c,1,a,1,b);
 		vector_gpu_sqrt(c,c);
 		#pragma endfuse	
+		
+		//wait for completion in order to accurately time it
 		clFinish(queue);	
 		double innerEnd = gettime();
+		
+		//transfer back results
 		vector_transfer(c,0);
 		clFinish(queue);
 	double outerEnd = gettime();
+	
+	//print execution time
 	printf("%f\t%f\t",outerEnd - outerStart, innerEnd - innerStart);
+	
+	//if check is enabled, self check results:
 	if(CHECK)
 	{
 		printf("\n");
@@ -79,6 +107,7 @@ int trianglefused(int size)
 int triangle(int size)
 {
 	if(DEBUG) perror("Triangle Fused");
+	//setup
 	Vector * a = newVector(size,16);
 	vector * b = newVector(size,16);
 	vector * c = newVector(size,16);
@@ -87,23 +116,28 @@ int triangle(int size)
 		a->cpu_vals[i] = i;
 		b->cpu_vals[i] = i;
 	}
+	
+	//data transfer
 	double outerStart = gettime();
 	vector_transfer(a,1);
 	vector_transfer(b,1);
 		clFinish(queue);
+		
+		//operations - unfused
 		double innerStart = gettime();
 		vector_gpu_mult(a,1,a,1,a);
 		vector_gpu_mult(b,1,b,1,b);
 		vector_gpu_add (c,1,a,1,b);
 		vector_gpu_sqrt(c,c);	
-		clFinish(queue);	
+		clFinish(queue);
+		//time and transfer back	
 		double innerEnd = gettime();
 		vector_transfer(c,0);
 		clFinish(queue);
 	double outerEnd = gettime();
 	printf("%f\t%f\t",outerEnd - outerStart, innerEnd - innerStart);
 
-	
+	//self check
 	if(CHECK)
 	{
 		printf("\n");
@@ -127,11 +161,15 @@ int triangle(int size)
 int distance2(int size)
 {
 	if(DEBUG) perror("Distance Fused");
+	
+	//setup 
 	Vector * x1 = newVector(size,16);
 	vector * x2 = newVector(size,16);
 	Vector * y1 = newVector(size,16);
 	vector * y2 = newVector(size,16);
 	vector * c = newVector(size,16);
+	
+	//assigment
 	for(int i = 0; i < size; i++)
 	{
 		x1->cpu_vals[i] = i;
@@ -139,12 +177,17 @@ int distance2(int size)
 		y1->cpu_vals[i] = i;
 		y2->cpu_vals[i] = i*2;		
 	}	
-	double outerStart = gettime();
+	
+
+	double outerStart = gettime();	
+	//transfer
 	vector_transfer(x1,1);
 	vector_transfer(x2,1);
 	vector_transfer(y1,1);
 	vector_transfer(y2,1);	
 		clFinish(queue);
+
+		//operations: fused
 		double innerStart = gettime();	
 		#pragma startfuse
 		vector_gpu_add(x1,1,x1,-1,x2);
@@ -156,6 +199,7 @@ int distance2(int size)
 		#pragma endfuse		
 		clFinish(queue);
 		double innerEnd = gettime();			
+		//transfer back to CPU
 		vector_transfer(c,0);
 		vector_transfer(y1,0);
 		vector_transfer(x1,0);
@@ -183,14 +227,18 @@ int distance2(int size)
 	return 0;
 }
 
+//distance calculates the distance between series of 2 x,y coordinates
 int distance(int size)
 {
 	if(DEBUG) perror("Distance");
+	//setup
 	Vector * x1 = newVector(size,16);
 	vector * x2 = newVector(size,16);
 	Vector * y1 = newVector(size,16);
 	vector * y2 = newVector(size,16);
 	vector * c = newVector(size,16);
+
+	//assigment
 	for(int i = 0; i < size; i++)
 	{
 		x1->cpu_vals[i] = i;
@@ -198,12 +246,16 @@ int distance(int size)
 		y1->cpu_vals[i] = i;
 		y2->cpu_vals[i] = i*2;		
 	}	
+
+	//transfer
 	double outerStart = gettime();
 	vector_transfer(x1,1);
 	vector_transfer(x2,1);
 	vector_transfer(y1,1);
 	vector_transfer(y2,1);	
 		clFinish(queue);
+
+		//do the operations
 		double innerStart = gettime();	
 		vector_gpu_add(x1,1,x1,-1,x2);
 		vector_gpu_add(y1,1,y1,-1,y2);
@@ -212,6 +264,8 @@ int distance(int size)
 		vector_gpu_add(c,1,x1,1,y1);	
 		vector_gpu_sqrt(c,c);
 		clFinish(queue);
+
+		//transfer
 		double innerEnd = gettime();			
 		vector_transfer(c,0);
 		vector_transfer(y1,0);
@@ -219,6 +273,8 @@ int distance(int size)
 		clFinish(queue);
 	double outerEnd = gettime();
 	printf("%f\t%f\t",outerEnd - outerStart, innerEnd - innerStart);	
+
+	//self check results
 	if(CHECK)
 	{
 		printf("\n");
@@ -242,7 +298,7 @@ int distance(int size)
 	return 0;			
 }
 
-
+//first few operations in the conjugate gradient method -- fused
 int cgStartFused(int size,Vector * result)
 {
 	if(DEBUG) perror("Start Fused");
