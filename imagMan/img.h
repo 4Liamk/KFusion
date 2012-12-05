644267338ad394689d94b8552f28db7bc6887ef5
Liam Kiemele <liamk@Beast.cs.uvic.ca>
2012-12-04 16:07:46 -0800

    added some documentation for image manipulation and main file for physics case study

--- imagMan/img.h
+++ imagMan/img.h
@@ -29,16 +29,25 @@ size_t getLocalSize(int global);
 
 /*
  type: ConvMatrix
- a convolution matrix type used
+ a convolution matrix type used when performing an image convolution
 */
 typedef struct convMatrix
 {
+		//gpu data
 		cl_mem GPU_Matrix;
+		
+		//cpu data
 		TYPE * CPU_Matrix;
+		
+		//dimension of the matrix
 		int dim;
+		
+		//divisor for the end result typically 9 or 25 depending on size
 		float div;
+		
+		//size of the matrix typicaly 9 or 25
 		int size;
-
+		//locality flag
 		int locality;
 }ConvMatrix;
 
@@ -47,33 +56,72 @@ typedef struct convMatrix
 */
 typedef struct image
 {
-		cl_mem GPU_Image;
-		TYPE * CPU_Image;
-		unsigned int width;
-		unsigned int height;	
-		int locality;
-		int type;
+		cl_mem GPU_Image; //gpu data
+		TYPE * CPU_Image; //cpu data
+		unsigned int width; //widt of image
+		unsigned int height; //height of image
+		int locality; //locality flag, host or device.  0 for host 1 for device
+		int type; //type of image RGB or HSV
 }Image;
 
 //conv Functions
+
+//create a convolution, must be deleted later with del_conv
 ConvMatrix * new_conv(int dim_);
+
+//combine two convolution functions to produce a third one approximately the combined width and height of the child matrices
+//must be deleted using del_conv
 ConvMatrix * merg_conv(ConvMatrix * one, ConvMatrix * two);
+
+//delete a convolution matrix
 void del_conv(ConvMatrix * tokill);
 
+/*
+  transfer a matrix to and from device. 
+  0 is for host. 
+  1 is for device
+*/
 void matrix_transfer(ConvMatrix * m, int dest);
-void matrix_print(char * prefix);
 
-void encode(char * message, char * image);
+/*Print a matrix.  DO NOT USE!*/
+//void matrix_print(ConvMatrix *m,char * prefix);
 
-char * decode(char * outfile, char * image);
-
+//Image creation functions
+/*
+  new_ImageFromFile(char * file)
+  load a png from file
+  returns an Image struct
+*/
 Image * new_ImageFromFile(char * file);
+
+/*
+  new_ImageFromWidthHeight(int w. int h)
+  creates a new image of width w and height h
+  image should be deleted using del_Image
+*/
 Image * new_ImageWidthHeight(int w, int h);
+
+/*
+   Copy and Image from another one
+   Must be deleted using del_Image
+*/
 Image * copy_Image(Image * copy);
+
+/*
+	delete an image
+*/
 void del_Image(Image * I);
 
 //data movement to/from GPU
+/*
+  Transfer an image to and from the target device.  0 is the CPU, device is 1
+  Uses lazy copying and as such, will not copy of locality is the same as destination
+*/
 void transfer(Image * I, int dest);			
+
+/*
+  save an Image I to file as a png.  This uses basic PNG settings and seems to work well
+*/
 void save(Image * I, char * file);
 
 //image manipulation functions
@@ -83,11 +131,12 @@ void convolve(Image * I, convMatrix * input);
 void colorize(Image * I, float color);
 
 //testing specific functions
+//Each one of these functions is a manual implementation of the automatic fusion produced by KFusion
 void bestCase(Image * I,int width,int height,float color);
 void avgCase(Image * I,Image * other, convMatrix * matrix);
 void complicated(Image * I,Image * other,convMatrix *  blur, convMatrix * sharpen);
 
-//image conversion stuff
+//image conversion operations
 void toHSV(Image * I);
 void toRGB(Image * I);
 void setFormat(Image * I,int type_);
--- physics/main.cpp
+++ physics/main.cpp
@@ -5,6 +5,13 @@
 TYPE prev;
 TYPE now;
 
+/**
+ * OpenCL Pool: This application accompishes a fairly simple pool implementation
+ * 
+*/
+
+
+//returns the current time as a double
 double gettime()
 {
 	struct timeval start;
@@ -12,19 +19,20 @@ double gettime()
 	return (double)start.tv_sec + start.tv_usec/1000000.0;
 }
 
+//runs one step of the simulation.  Uncomment if you would like to fuse it.  Kfusion does not allow you to repeat yourself: only one unqiue combination of functions may be fused once.
 void runstep(Game * g)
 {
 	TYPE timestep = 0.0001;
-	/*
-	#pragma startfuse	
+	
+	//#pragma startfuse	
 	collideBalls(g,timestep);	
 	collidePockets(g);
 	collideWalls(g);
 	moveBalls(g,timestep);	
-	#pragma endfuse	
-	*/
+	//#pragma endfuse
 }
 
+//run the game when the OpenGL context is not rendering:
 void Idle()
 {
 	if(not stopped)
@@ -39,6 +47,7 @@ void Idle()
 
 int main(int argc, char ** argv)
 {
+	//this does not require any arguments
 	prev = gettime();
 	now = gettime();
 	if(DEBUG) perror("Starting Up!");
@@ -46,15 +55,19 @@ int main(int argc, char ** argv)
 	init(atoi(argv[1]),atoi(argv[2]));
 	glutIdleFunc(Idle);
 	testfunc = runstep;
+	
+	//if you would like to play the game proper, uncommment this:
 	//run();
 
-
+	//actual test code to obtain results:
+	//If you want to play the game, comment out the following code with some /* */
 	g.cue.y = .5;
 	//impulse(&g);
 	TYPE timestep = 0.0001;
 	for(int i = 0; i < 1000; i++)
 	{
 		{
+			//basic excecution unfused.  Inscoped to allow for similarly named variables as second case
 			clFinish(queue);
 			TYPE start = gettime();
 				collideBalls(&g,timestep);	
@@ -67,6 +80,7 @@ int main(int argc, char ** argv)
 		}
 		{
 			clFinish(queue);
+			//second case: This one will be fused.
 			TYPE start = gettime();	
 			#pragma startfuse	
 				collideBalls(&g,timestep);	
